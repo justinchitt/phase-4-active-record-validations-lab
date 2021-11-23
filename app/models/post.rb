af328aeb947ee59c23b47df60c0a5882c696d066
Justin Chittarath <justinchitt@gmail.com>
2021-11-23 10:22:35 -0700

    commit tree 5a448c2a626783da6a274f326a0860418e1a3744

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :true_facts
+    
+    def true_facts
+        unless (title.match?(/Won't Believe/) || title.match?(/Secret/) || title.match?(/Top [number]/) || title.match?(/Guess/)) == true
+            errors.add(:title, "Invalid")
+        end
+    end
 end
